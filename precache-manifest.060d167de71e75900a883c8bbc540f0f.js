00305eb759ad9e3855431f4fa14b00996ecf45ee
unknown <mukherjeerivu7@gmail.com>
2020-07-29 11:38:47 +0530

    Updates

---
+++ precache-manifest.060d167de71e75900a883c8bbc540f0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c177694b33e2908117402d54b459180",
+    "url": "/react-tailwind-pixabay-gallery/index.html"
+  },
+  {
+    "revision": "63e2b968f4d6863f8249",
+    "url": "/react-tailwind-pixabay-gallery/static/css/main.3f8e1b76.chunk.css"
+  },
+  {
+    "revision": "64be53b2dd1f5b630a0e",
+    "url": "/react-tailwind-pixabay-gallery/static/js/2.c1d6216f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-pixabay-gallery/static/js/2.c1d6216f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e2b968f4d6863f8249",
+    "url": "/react-tailwind-pixabay-gallery/static/js/main.9e87a7f9.chunk.js"
+  },
+  {
+    "revision": "a252dd38e0a17a672fdf",
+    "url": "/react-tailwind-pixabay-gallery/static/js/runtime-main.d049f35b.js"
+  }
+]);
